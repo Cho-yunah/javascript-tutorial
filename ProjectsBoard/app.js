3f8c768e3f836cef80e05dd400851320dc66d7c8
Yunah <favoryun12@gmail.com>
2021-09-16 15:39:38 +0900

    filter button 추가

---
+++ ProjectsBoard/app.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    title: 'pungdung',
+    category: 'collaboration',
+    platform: 'app',
+    img: '#',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos quo fuga quidem nemo accusantium laboriosam laborum quasi veritatis molestiae? Id cumque enim temporibus hic, distinctio quae repudiandae animi architecto!',
+  },
+  {
+    id: 2,
+    title: 'K-bnb',
+    category: 'collaboration',
+    platform: 'web',
+    img: '#',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos quo fuga quidem nemo accusantium laboriosam laborum quasi veritatis molestiae? Id cumque enim temporibus hic, distinctio quae repudiandae animi architecto!',
+  },
+]
+
+const sectionCenter = document.querySelector('section-center')
+
+window.addEventListener('DOMContentLoaded', () => {})
